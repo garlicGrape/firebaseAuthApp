293336a629f9425ad942c722540d41d04be2be38
garlicGrape <sk7374@nyu.edu>
2023-08-17 17:14:41 -0400

    added html pages that I will eventually use

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-const { v4: uuidv4 } = require('uuid');
+const uuid = require('uuid');
 const jwt = require("jsonwebtoken");
 
 const bcrypt = require("bcrypt");
@@ -7,37 +7,98 @@ const saltRounds = 10;
 
 
 
-exports.postSignup = async (req, res, next) => {
+// exports.postSignup = async (req, res, next) => {
      
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     const confirmPassword = req.body.confirmPassword;
+
+//     if (!email || !password || !confirmPassword) {
+//         // no username or password received in the POST body... send an error
+//         res
+//           .status(401)
+//           .json({ success: false, message: `no username or password supplied.` });
+//       }
+//       if (password != confirmPassword) {
+//         //password doesn't match the password check
+//         res.status(401).json({ success: false, message: `passwords don't match` });
+//       }
+
+//       bcrypt
+//         .hash(password, saltRounds)
+//         .then(hash => {
+//             const newUser = new User({ email: email, password: hash });
+//             console.log(newUser);
+//             return newUser.save();
+//         })
+//         .then(result => {
+//             return res.json({ success: true, email: req.body.email });
+//         })
+//         .catch(err => {
+//             console.log(err);
+//         })
+// };
+
+exports.postSignup = async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-
+  
     if (!email || !password || !confirmPassword) {
-        // no username or password received in the POST body... send an error
-        res
-          .status(401)
-          .json({ success: false, message: `no username or password supplied.` });
-      }
-      if (password != confirmPassword) {
-        //password doesn't match the password check
-        res.status(401).json({ success: false, message: `passwords don't match` });
-      }
-
-      bcrypt
-        .hash(password, saltRounds)
-        .then(hash => {
-            const newUser = new User({ email: email, password: hash });
-            console.log(newUser);
-            return newUser.save();
-        })
-        .then(result => {
-            return res.json({ success: true, email: req.body.email });
-        })
-        .catch(err => {
-            console.log(err);
-        })
-};
+        res.status(401).json({
+        success: false,
+        message: `No username or password supplied.`,
+      });
+    }
+    if (password !== confirmPassword) {
+        res.status(401).json({
+        success: false,
+        message: `Passwords don't match.`,
+      });
+    }
+  
+    // Generate a UID for the user (using uuid library)
+    const uid = uuid.v4();
+  
+    bcrypt
+      .hash(password, saltRounds)
+      .then((hash) => {
+        const newUser = new User({ uid: uid, email: email, password: hash }); // Store the generated UID in your MongoDB user document
+        console.log(newUser);
+        return newUser.save();
+      })
+      .then((result) => {
+        // Create a custom token for Firebase Authentication
+        admin
+          .auth()
+          .createCustomToken(uid)
+          .then((customToken) => {
+            // Send the custom token to Firebase for authentication
+            return admin.auth().signInWithCustomToken(customToken);
+          })
+          .then((firebaseUser) => {
+            // Continue with your logic, like generating your own JWT token
+            const token = jwt.sign(
+              { uid: uid, email: email },
+              process.env.JWT_SECRET
+            );
+            return res.json({
+              success: true,
+              email: req.body.email,
+              token: token, // Include the generated JWT token in the response
+            });
+          })
+          .catch((error) => {
+            console.error('Error creating custom token:', error);
+            res.status(500).json({ success: false, message: 'Authentication error' });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ success: false, message: 'Signup error' });
+      });
+  };
+  
 
 exports.postLogin = async (req, res, next) => {
     const email = req.body.email;
--- models/user.js
+++ models/user.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const userSchema = new mongoose.Schema({
-  // _id: { type: String, default: uuidv4 }, // Use UUID as the _id
+  _id: { type: String, default: uuidv4 }, // Use UUID as the _id
   email: { type: String, required: true, unique: true },
+  firstname: { type: String, required: true},
+  lastname: { type: String, required: true},
   password: { type: String, required: true },
-  // groupCode: { type: String, required: false },
+  groupCode: { type: String, required: false },
   // ... other user-specific fields
 });
 
